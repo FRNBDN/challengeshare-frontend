2c85bd309435cee8658039999a206123cf1a8397
FRNBDN <francesco.badini94@gmail.com>
2023-04-11 12:48:05 +0000

    update challenges to dares, add route to create

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import DareCreateForm from "./pages/dares/DareCreateForm";
 
 function App() {
   return (
@@ -13,9 +14,10 @@ function App() {
       <Container>
         <Routes>
           <Route exact path="/" element={<h1>Submissions</h1>} />
-          <Route exact path="/challenges" element={<h1>Challenges</h1>} />
+          <Route exact path="/dares" element={<h1>Dares</h1>} />
           <Route exact path="/signin" element={<SignInForm />} />
           <Route exact path="/signup" element={<SignUpForm />} />
+          <Route exact path="/dares/create" element={<DareCreateForm />} />
           <Route path="*" element={<h1>Page not found :(</h1>} />
         </Routes>
       </Container>
