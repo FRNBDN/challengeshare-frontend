e7de08d935ff56839b771c2487092f0bc5a03eaa
FRNBDN <francesco.badini94@gmail.com>
2023-04-16 13:09:47 +0000

    add daresfeed routes for all, followed users/dares

--- src/App.js
+++ src/App.js
@@ -7,15 +7,44 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import DareCreateForm from "./pages/dares/DareCreateForm";
 import DarePage from "./pages/dares/DarePage";
+import DaresFeedPage from "./pages/dares/DaresFeedPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container>
         <Routes>
           <Route exact path="/" element={<h1>Submissions</h1>} />
-          <Route exact path="/dares" element={<h1>Dares</h1>} />
+          <Route
+            exact
+            path="/dares"
+            element={<DaresFeedPage messsage="No results found" />}
+          />
+          <Route
+            exact
+            path="/dares/byfollowed"
+            element={
+              <DaresFeedPage
+                messsage="No results found for followed users"
+                filter={`owner__ufollowed__owner__profile=${profile_id}&`}
+              />
+            }
+          />
+          <Route
+            exact
+            path="/dares/following"
+            element={
+              <DaresFeedPage
+                messsage="No results found for followed dares"
+                filter={`cfollowed__owner__profile=${profile_id}&ordering=cfollowed__created_at`}
+              />
+            }
+          />
           <Route exact path="/signin" element={<SignInForm />} />
           <Route exact path="/signup" element={<SignUpForm />} />
           <Route exact path="/dares/create" element={<DareCreateForm />} />
