132e89484364826231a3e4506f80c89af9787fce
FRNBDN <francesco.badini94@gmail.com>
2023-04-16 14:42:51 +0000

    add display, fetching dares, filters

--- src/pages/dares/DaresFeedPage.js
+++ src/pages/dares/DaresFeedPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
@@ -6,14 +6,57 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import styles from "../../App.module.css";
+import { Link, useLocation } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import Dare from "./Dare";
+import Asset from "../../components/Asset";
 
-function DaresFeedPage() {
+function DaresFeedPage({ message, filter = "" }) {
+  const [dares, setDares] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const fetchDares = async () => {
+      try {
+        const { data } = await axiosReq.get(`/challenges/?${filter}`);
+        setDares(data);
+        setHasLoaded(true);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    setHasLoaded(false);
+    fetchDares();
+  }, [filter, pathname]);
+
   return (
     <>
       <h1 className={styles.BrandFont}>Dares</h1>
       <Row className="h-100">
+        <div>
+          <Link to="/dares/byfollowed">Dares by Followed Users</Link>
+          <Link to="/dares/following">Followed Dares</Link>
+          <Link to="/dares">All</Link>
+        </div>
         <Col>
-          <p>List of posts here</p>
+          {hasLoaded ? (
+            <>
+              {dares.results.length ? (
+                dares.results.map((dare) => (
+                  <Dare key={dare.id} {...dare} setDares={setDares} />
+                ))
+              ) : (
+                <Container>
+                  <Asset message={message} />
+                </Container>
+              )}
+            </>
+          ) : (
+            <Container>
+              <Asset spinner />
+            </Container>
+          )}
         </Col>
       </Row>
     </>
