08ea45d6c61e22b4cb72cc57cf79655e7681837f
FRNBDN <francesco.badini94@gmail.com>
2023-04-29 14:16:29 +0000

    create submission page, add boilerplate code

---
+++ src/pages/submissions/SubmissionPage.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import { Link, useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import { Col } from "react-bootstrap";
+
+import appStyles from "../../App.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Avatar from "../../components/Avatar";
+import Submission from "./Submission";
+
+function SubmissionPage() {
+  const { id } = useParams();
+  const [submission, setSubmission] = useState({ results: [] });
+  const currentUser = useCurrentUser();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: submission }] = await Promise.all([
+          axiosReq.get(`submissions/${id}`),
+        ]);
+        setSubmission({ results: [submission] });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    handleMount();
+  }, [id]);
+
+  return (
+    <Row>
+      <h1 className={appStyles.BrandFont}>Submission / Detail</h1>
+      <Col md={9}>
+        <Row className="d-block d-md-none">Top Bar</Row>
+        <Row className="h-100">
+          <Col>
+            <Container className="px-0">
+              <Submission
+                {...submission.results[0]}
+                setSubmissions={setSubmission}
+              />
+            </Container>
+            <Container>Reveiws here</Container>
+          </Col>
+        </Row>
+      </Col>
+      <Col md={3} className="d-none d-md-block">
+        <Row>
+          <Container className={`${appStyles.Box} pb-1 mb-2`}>
+            <div>
+              <h5 className="mb-0 mt-1">
+                <i className="fa-solid fa-fire-flame-curved"></i> Dares
+              </h5>
+            </div>
+            <hr className="m-1"></hr>
+            <div className="d-flex flex-column"></div>
+          </Container>
+        </Row>
+        <Row>
+          <Container className={`${appStyles.Box} pb-1 mb-2`}>
+            <div>
+              <h5 className="mb-0 mt-1">
+                <i className="fa-solid fa-fire-flame-curved"></i> Profiles
+              </h5>
+            </div>
+            <hr className="m-1"></hr>
+            <div className="d-flex flex-column"></div>
+          </Container>
+        </Row>
+      </Col>
+    </Row>
+  );
+}
+
+export default SubmissionPage;
