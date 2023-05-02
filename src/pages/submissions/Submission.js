d00da4708a9001f24624a21652c5475238ad0a65
FRNBDN <francesco.badini94@gmail.com>
2023-05-02 09:00:14 +0000

    import reviewcreateform component

--- src/pages/submissions/Submission.js
+++ src/pages/submissions/Submission.js
@@ -14,6 +14,7 @@ import Collapse from "react-bootstrap/Collapse";
 import { Link, useLocation } from "react-router-dom";
 import { axiosReq, axiosRes } from "../../api/axiosDefaults";
 import Asset from "../../components/Asset";
+import ReviewCreateForm from "../reviews/ReviewCreateForm";
 import appStyles from "../../App.module.css";
 import styles from "../../styles/Submission.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
@@ -25,17 +26,22 @@ const Submission = (props) => {
     text,
     profile_id,
     profile_image,
+    challenge,
     status,
     reviews,
     created_at,
-    setSubmissions,
+    setSubmission,
     updated_at,
     Feed,
+    has_reviewed,
   } = props;
 
   const currentUser = useCurrentUser();
+  const curr_user_img = currentUser?.profile_image;
   const is_owner = currentUser?.username === owner;
   const [uploads, setUploads] = useState({ results: [] });
+  const [dare, setDare] = useState({});
+  const [review, setReview] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
   const [open, setOpen] = useState(false);
@@ -43,8 +49,13 @@ const Submission = (props) => {
   useEffect(() => {
     const fetchUploads = async () => {
       try {
-        const { data } = await axiosReq.get(`/uploads/?submission=${id}`);
-        setUploads(data);
+        const [{ data: uploads }, { data: dare }] = await Promise.all([
+          axiosReq.get(`/uploads/?submission=${id}`),
+          axiosReq.get(`/challenges/${challenge}`),
+        ]);
+        setUploads(uploads);
+        setDare(dare);
+
         setHasLoaded(true);
       } catch (error) {
         console.log(error);
@@ -52,7 +63,7 @@ const Submission = (props) => {
     };
     setHasLoaded(false);
     fetchUploads();
-  }, [id, pathname]);
+  }, [id, pathname, challenge]);
 
   return (
     <Card className={`mb-2 ${appStyles.Card} `}>
@@ -77,6 +88,22 @@ const Submission = (props) => {
           </Col>
         </Row>
       </Card.Body>
+      <Card.Body>
+        <Link to={`/dares/${dare.id}`}>
+          <Container>
+            <h6 className={styles.BorderText}>Dare</h6>
+            <Row className={styles.ThickBorder}>
+              <Col xs={4}>
+                <h5>{dare.title}</h5>
+              </Col>
+              <Col xs={8}>
+                <h6>Description:</h6>
+                <span>{dare.description}</span>
+              </Col>
+            </Row>
+          </Container>
+        </Link>
+      </Card.Body>
 
       <Card.Body>
         <div className="d-flex justify-content-between align-items-center mt-0">
@@ -89,7 +116,7 @@ const Submission = (props) => {
           )}
         </div>
         <Row>
-          <Container>
+          <Container className={styles.ImageField}>
             {hasLoaded ? (
               <>
                 {uploads.results.length ? (
@@ -97,9 +124,10 @@ const Submission = (props) => {
                     interval={null}
                     variant="dark"
                     controls={uploads.results.length === 1 ? false : true}
+                    indicators={uploads.results.length === 1 ? false : true}
                   >
                     {uploads.results.map((upload, index) => (
-                      <Carousel.Item>
+                      <Carousel.Item key={upload.id}>
                         <img
                           className="d-block w-100 "
                           src={upload.upload}
@@ -120,7 +148,63 @@ const Submission = (props) => {
           </Container>
         </Row>
         <Row>
-          <Container> Review button here</Container>
+          <Container>
+            {has_reviewed ? (
+              <OverlayTrigger
+                placement="top"
+                overlay={
+                  <Tooltip>You can only have one review per submission</Tooltip>
+                }
+              >
+                <span>
+                  <Button
+                    aria-controls="review-form-collapse"
+                    disabled
+                    className={`${appStyles.Button} ${appStyles.disabled} ${appStyles.BrandFont}`}
+                  >
+                    Reviewed
+                  </Button>
+                </span>
+              </OverlayTrigger>
+            ) : currentUser ? (
+              <Button
+                onClick={() => setOpen(!open)}
+                aria-controls="review-form-collapse"
+                aria-expanded={open}
+                className={`${appStyles.Button} ${appStyles.BrandFont}`}
+              >
+                {!open ? "Review Submission" : "Hide Form"}
+              </Button>
+            ) : (
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>Sign in to leave reviews</Tooltip>}
+              >
+                <span>
+                  <Button
+                    aria-controls="review-form-collapse"
+                    disabled
+                    className={`${appStyles.Button} ${appStyles.disabled} ${appStyles.BrandFont}`}
+                  >
+                    Review Submission
+                  </Button>
+                </span>
+              </OverlayTrigger>
+            )}
+
+            <Collapse in={open}>
+              <div id="review-form-collapse">
+                <ReviewCreateForm
+                  profile_id={currentUser?.profile_id}
+                  profileImage={curr_user_img}
+                  submission={id}
+                  setSubmission={setSubmission}
+                  setReview={setReview}
+                  dare_id={dare.id}
+                />
+              </div>
+            </Collapse>
+          </Container>
           <span className={`d-flex justify-content-end ${styles.Edit}`}>
             {is_owner && (
               <Link to={`/submissions/${id}/edit`}>
