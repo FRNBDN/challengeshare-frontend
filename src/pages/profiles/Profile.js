807f80498f38405bab5d6052bd665e9347d93bf3
FRNBDN <francesco.badini94@gmail.com>
2023-05-04 15:00:45 +0000

    add profile component, style jsx code

---
+++ src/pages/profiles/Profile.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styles from "../../styles/Profile.module.css";
+import appStyles from "../../App.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Avatar from "../../components/Avatar";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Profile = (props) => {
+  const { profile, mobile } = props;
+  const { id, following_id, image, owner } = profile;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  return (
+    <div
+      className={`mt-1 d-flex align-items-center ${mobile && "flex-column"} ${
+        appStyles.Box
+      }`}
+    >
+      <div>
+        <Link className="align-self-center" to={`/profiles/${id}`}>
+          <Avatar src={image} height={mobile ? 55 : 30} />
+        </Link>
+      </div>
+      <div className={`mx-2 ${!mobile && "flex-fill"}`}>
+        <span className={`flex-grow ${appStyles.BrandFont}`}>{owner}</span>
+      </div>
+      <div className={`text-right`}>
+        {currentUser &&
+          (is_owner ? (
+            <Button
+              disabled
+              className={`${appStyles.Button} ${appStyles.BrandFont} ${appStyles.disabled}`}
+            >
+              follow
+            </Button>
+          ) : following_id ? (
+            <Button
+              className={`${appStyles.Button} ${appStyles.BrandFont}`}
+              onClick={() => {}}
+            >
+              unfollow
+            </Button>
+          ) : (
+            <Button
+              className={`${appStyles.Button} ${appStyles.BrandFont}`}
+              onClick={() => {}}
+            >
+              follow
+            </Button>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
