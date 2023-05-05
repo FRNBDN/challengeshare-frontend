ab39303647a07cd20c06474fc6b49490b049c870
FRNBDN <francesco.badini94@gmail.com>
2023-05-05 13:53:22 +0000

    add profilepage userdata logic, jsx

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -17,10 +17,15 @@ import {
   useProfileData,
   useSetProfileData,
 } from "../../contexts/ProfileDataContext";
-import { Button, Card, Form, Image } from "react-bootstrap";
+import { Button, Card, Form, Image, NavLink } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Dare from "../dares/Dare";
+import { fetchMoreData } from "../../utils/utils";
+import Submission from "../submissions/Submission";
+import Profile from "./Profile";
 
-function ProfilePage() {
+function ProfilePage({ message, model, filter }) {
   const [hasLoaded, setHasLoaded] = useState(false);
   const currentUser = useCurrentUser();
   const { id } = useParams();
@@ -28,29 +33,33 @@ function ProfilePage() {
   const { pageProfile } = useProfileData();
   const [profile] = pageProfile.results;
   const is_owner = currentUser?.username === profile?.owner;
+  const [profileModels, setProfileModels] = useState({ results: [] });
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [{ data: pageProfile }] = await Promise.all([
-          axiosReq.get(`/profiles/${id}`),
-        ]);
+        const [{ data: pageProfile }, { data: profileModels }] =
+          await Promise.all([
+            axiosReq.get(`/profiles/${id}`),
+            axiosReq.get(`/${model}/?${filter}=${id}`),
+          ]);
         setProfileData((prevState) => ({
           ...prevState,
           pageProfile: { results: [pageProfile] },
         }));
+        setProfileModels(profileModels);
         setHasLoaded(true);
       } catch (error) {
         console.log(error);
       }
     };
     fetchData();
-  }, [id, setProfileData]);
+  }, [id, setProfileData, message]);
 
   const mainProfile = (
     <>
       <Container className={appStyles.Box}>
-        <Row noGutters className="px-0 text-center d-flex align-items-center">
+        <Row className="px-0 text-center d-flex align-items-center">
           <Col xs={5} className="text-start">
             <Row>
               <Image roundedCircle src={profile?.image} className="px-0 mx-0" />
@@ -67,9 +76,7 @@ function ProfilePage() {
             </Row>
           </Col>
         </Row>
-        <Row className="my-1 d-flex justify-content-center">
-          {profile?.bio} asdasd
-        </Row>
+        <Row className="my-1 d-flex justify-content-center">{profile?.bio}</Row>
         <hr />
         <Row>
           <Col xs={6}>
@@ -115,21 +122,121 @@ function ProfilePage() {
         <Col
           className={`d-flex flex-wrap flex-md-nowrap flex-column flex-md-row`}
         >
-          <Button className={`${appStyles.Button} flex-fill`}>
+          <Link
+            to={`/profiles/${id}`}
+            className={`${appStyles.Button} flex-fill`}
+          >
             Sub
             <span className="d-inline-block d-md-none d-lg-inline-block">
               missions
             </span>
-          </Button>
-          <Button className={`${appStyles.Button} flex-fill `}>Dares</Button>
+          </Link>
+          <Link
+            to={`/profiles/${id}/dares`}
+            className={`${appStyles.Button} flex-fill `}
+          >
+            Dares
+          </Link>
 
-          <Button className={`${appStyles.Button} flex-fill`}>Followers</Button>
-          <Button className={`${appStyles.Button} flex-fill`}>Following</Button>
+          <Link
+            to={`/profiles/${id}/followers`}
+            className={`${appStyles.Button} flex-fill`}
+          >
+            Followers
+          </Link>
+          <Link
+            to={`/profiles/${id}/following`}
+            className={`${appStyles.Button} flex-fill`}
+          >
+            Following
+          </Link>
         </Col>
       </Row>
       <Container className={`${appStyles.Box}`}>
         <Row>
-          <Col>Content here</Col>
+          <Col>
+            {
+              model === "challenges" ? (
+                profileModels.results.length ? (
+                  <InfiniteScroll
+                    children={profileModels.results.map((dare) => (
+                      <Dare
+                        key={dare.id}
+                        {...dare}
+                        setDares={setProfileModels}
+                      />
+                    ))}
+                    dataLength={profileModels.results.length}
+                    loader={<Asset spinner />}
+                    hasMore={!!profileModels.next}
+                    next={() => {
+                      fetchMoreData(profileModels, setProfileModels);
+                    }}
+                  />
+                ) : (
+                  <Asset message={message} />
+                ) //dares above
+              ) : model === "submissions" ? (
+                profileModels.results.length ? (
+                  <InfiniteScroll
+                    children={profileModels.results.map((submission) => (
+                      <Submission
+                        key={submission.id}
+                        {...submission}
+                        setSubmissions={setProfileModels}
+                      />
+                    ))}
+                    dataLength={profileModels.results.length}
+                    loader={<Asset spinner />}
+                    hasMore={!!profileModels.next}
+                    next={() => {
+                      fetchMoreData(profileModels, setProfileModels);
+                    }}
+                  />
+                ) : (
+                  <Asset message={message} />
+                ) //subs above
+              ) : filter === "followed__profile" ? (
+                profileModels.results.length ? (
+                  <InfiniteScroll
+                    children={profileModels.results.map((profile) => (
+                      <Profile
+                        key={profile.id}
+                        {...profile}
+                        setProfile={setProfileModels}
+                      />
+                    ))}
+                    dataLength={profileModels.results.length}
+                    loader={<Asset spinner />}
+                    hasMore={!!profileModels.next}
+                    next={() => {
+                      fetchMoreData(profileModels, setProfileModels);
+                    }}
+                  />
+                ) : (
+                  <Asset message={message} />
+                ) //followers above
+              ) : profileModels.results.length ? (
+                <InfiniteScroll
+                  children={profileModels.results.map((profile) => (
+                    <Profile
+                      key={profile.id}
+                      {...profile}
+                      setProfile={setProfileModels}
+                    />
+                  ))}
+                  dataLength={profileModels.results.length}
+                  loader={<Asset spinner />}
+                  hasMore={!!profileModels.next}
+                  next={() => {
+                    fetchMoreData(profileModels, setProfileModels);
+                  }}
+                />
+              ) : (
+                <Asset message={message} />
+              ) //following above
+            }
+          </Col>
         </Row>
       </Container>
     </>
