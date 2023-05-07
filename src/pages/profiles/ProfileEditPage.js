02c5085deee2f45c199cb66d81931527cee67881
FRNBDN <francesco.badini94@gmail.com>
2023-05-07 08:14:17 +0000

    fix profileeditpage responsive layout

--- src/pages/profiles/ProfileEditPage.js
+++ src/pages/profiles/ProfileEditPage.js
@@ -1,35 +1,33 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import TopProfiles from "./TopProfiles";
 import ProfileEditForm from "./ProfileEditForm";
 import UsernameForm from "./UsernameForm";
 import UserPasswordForm from "./UserPasswordForm";
 
 import appStyles from "../../App.module.css";
+import formStyles from "../../styles/Forms.module.css";
 import { Link } from "react-router-dom";
 
 const ProfileEditPage = () => {
   return (
     <>
-      <h1>Profile</h1>
+      <h1 className="pb-2">Profile / Edit</h1>
       <Row>
         <TopProfiles mobile />
-        <Col className="p-0 " md={9}>
+        <Col className="p-0" md={9}>
           <Row>
-            <Col>
+            <Col xs={12} md={4} className="mt-1 mt-md-0">
               <UsernameForm />
-            </Col>
-
-            <Col>
               <UserPasswordForm />
             </Col>
-          </Row>
 
-          <Row>
-            <ProfileEditForm />
+            <Col xs={12} md={8} className="pe-md-4 ps-md-0">
+              <ProfileEditForm />
+            </Col>
           </Row>
         </Col>
-        <Col md={3} className="d-none d-md-block ps-2 ">
+        <Col md={3} className="d-none d-md-block">
           <Row>
             <div className="d-flex flex-column px-0 pb-3 ">
               <Link
